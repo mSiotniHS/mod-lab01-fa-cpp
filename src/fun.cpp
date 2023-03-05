0c2929d12ea7a6dd44091f854fc052f47ec2bfa5
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-05 22:41:57 +0300

    Реализовал `faStr3`

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,7 @@
 // Copyright 2022 UNN-IASR
 #include <cstring>
 #include <cctype>
+#include <cmath>
 #include "fun.h"
 
 using namespace std;
@@ -75,5 +76,28 @@ unsigned int faStr2(const char *str) {
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    size_t length = strlen(str);
+
+    bool inWord = false;
+    unsigned int wordCount = 0;
+    unsigned int lengthSum = 0;
+
+    for (size_t i = 0; i < length; i++) {
+        char character = str[i];
+
+        if (!inWord && character != space) {
+            inWord = true;
+            wordCount++;
+        }
+
+        if (inWord && character != space) {
+            lengthSum++;
+        }
+
+        if (inWord && character == space) {
+            inWord = false;
+        }
+    }
+
+    return round((float) lengthSum / wordCount);
 }
